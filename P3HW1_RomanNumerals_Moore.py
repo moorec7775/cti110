28a77d653362b655b5a21f89fd9472457d093308
moorec7775 <43920882+moorec7775@users.noreply.github.com>
2018-10-07 00:39:00 -0400

    Add files via upload
    Roman numerals

---
+++ P3HW1_RomanNumerals_Moore.py
@@ -0,0 +1,31 @@
+# CTI-110
+# P3HW1 - Roman Numerals
+# Colin Moore
+# 10/06/2018
+
+# Convert numbers into their Roman numeral form
+userNumber = int( input('Please enter a number'))
+if userNumber == 1:
+    print('I')
+elif userNumber == 2:
+    print('II')
+elif userNumber == 3:
+    print('III')
+elif userNumber == 4:
+    print('IV')
+elif userNumber == 5:
+    print('V')
+elif userNumber == 6:
+    print('VI')
+elif userNumber == 7:
+    print('VII')
+elif userNumber == 8:
+    print('VIII')
+elif userNumber == 9:
+    print('IX')
+elif userNumber == 10:
+    print('X')
+else:
+    print('Error: Restart program. Numbers between 1 and 10 only.')
+  
+
